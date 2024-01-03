b1a86482c2c2159991b4793432ae91f07ac39f34
ehdtjs0612 <inko51366@naver.com>
2024-01-03 21:15:28 +0900

    feat: add error handling middleware

---
+++ src/middlewares/error-handling.ts
@@ -0,0 +1,18 @@
+import { ErrorRequestHandler } from 'express';
+import { ResponseEntity } from '../utils/response-entity';
+import { CustomError } from '../utils/custom-error';
+import { HttpStatus } from '../utils/http-status';
+
+const errorHandling: ErrorRequestHandler = (error: CustomError | Error, req, res, next) => {
+    console.error(error);
+
+    if (error instanceof Error) {
+        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(ResponseEntity.ERROR());
+    }
+
+    return res
+        .status(error.statusCode)
+        .send(ResponseEntity.ERROR_WITH(error.statusCode, error.message));
+};
+
+export default errorHandling;
