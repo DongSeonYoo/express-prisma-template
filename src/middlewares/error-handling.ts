ee3c3c0fd180d1cfd0d67f170defaa2498854e91
ehdtjs0612 <inko51366@naver.com>
2024-01-19 23:58:08 +0900

    refactor: 에러 미들웨어 함수로 분리

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,19 +1,19 @@
-import { ErrorRequestHandler } from 'express';
 import { ResponseEntity } from '../utils/modules/response-entity';
 import { CustomError } from '../utils/modules/custom-error';
 import { HttpStatus } from '../utils/modules/http-status';
+import { Request, Response, NextFunction } from 'express';
 
-const errorHandling: ErrorRequestHandler = (error: CustomError | Error, req, res, next) => {
-  // 개발환경 전용
-  console.error(error);
+export default function () {
+  return (error: CustomError | Error, req: Request, res: Response, next: NextFunction) => {
+    // 개발환경 전용
+    console.error(error);
 
-  if (error instanceof Error) {
-    return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(ResponseEntity.ERROR());
-  }
+    if (error instanceof Error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(ResponseEntity.ERROR());
+    }
 
-  return res
-    .status(error.statusCode)
-    .send(ResponseEntity.ERROR_WITH(error.statusCode, error.message));
-};
-
-export default errorHandling;
+    return res
+      .status(error.statusCode)
+      .send(ResponseEntity.ERROR_WITH(error.statusCode, error.message));
+  };
+}
