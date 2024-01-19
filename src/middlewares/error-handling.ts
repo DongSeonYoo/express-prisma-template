b502004f6bbaa41c1a951002b46f5542b6a24654
ehdtjs0612 <inko51366@naver.com>
2024-01-19 23:16:09 +0900

    refactor: 조건문 리팩터링

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -4,9 +4,10 @@ import { CustomError } from '../utils/modules/custom-error';
 import { HttpStatus } from '../utils/modules/http-status';
 
 const errorHandling: ErrorRequestHandler = (error: CustomError | Error, req, res, next) => {
+  // 개발환경 전용
   console.error(error);
 
-  if (!(error instanceof CustomError)) {
+  if (error instanceof Error) {
     return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(ResponseEntity.ERROR());
   }
 
