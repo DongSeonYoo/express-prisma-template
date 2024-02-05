ee65ac2dacd8e3939f275b9123923112cf81f6fe
ehdtjs0612 <inko51366@naver.com>
2024-02-06 00:33:56 +0900

    feat: refactor custom error (add reason property in BadRequesetException use for when catched exception at validate middleware)

--- src/utils/modules/custom-error.module.ts
+++ src/utils/modules/custom-error.module.ts
@@ -1,37 +1,39 @@
-import { HttpStatus } from './http-status';
+import { HttpStatus } from './http-status.module';
 
 export class CustomError {
   statusCode: number;
   message: string;
+  reason?: any;
 
-  constructor(message: string) {
+  constructor(message: string, reason?: any) {
     this.message = message;
   }
 }
 
 export class BadRequestException extends CustomError {
-  constructor(message: string) {
-    super(message);
+  constructor(message: string = 'BadRequestException', reason?: any) {
+    super(message, reason);
     this.statusCode = HttpStatus.BAD_REQUEST;
+    this.reason = reason;
   }
 }
 
 export class UnauthorizedException extends CustomError {
-  constructor(message: string) {
+  constructor(message: string = 'UnauthorizedException') {
     super(message);
     this.statusCode = HttpStatus.UNAUTHORIZED;
   }
 }
 
 export class NotFoundException extends CustomError {
-  constructor(message: string) {
+  constructor(message: string = 'NotFoundException') {
     super(message);
     this.statusCode = HttpStatus.NOT_FOUND;
   }
 }
 
 export class ForbiddenException extends CustomError {
-  constructor(message: string) {
+  constructor(message: string = 'ForbiddenException') {
     super(message);
     this.statusCode = HttpStatus.FORBIDDEN;
   }
