6b4bcb5486ae417a3808c8e8cb23798919ca87eb
ehdtjs0612 <inko51366@naver.com>
2024-02-07 20:52:41 +0900

    rename: rename jwt to jsonwebtoken

--- src/types/jsonwebtoken.d.ts
+++ src/types/jsonwebtoken.d.ts
@@ -3,7 +3,6 @@ import jwt from 'jsonwebtoken';
 declare module 'jsonwebtoken' {
   export interface LoginTokenPayload {
     userIdx: number;
-    // add more payload options
   }
   export interface JwtPayload extends jwt.LoginTokenPayload {}
 }
