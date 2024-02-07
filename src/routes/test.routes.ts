faf44e945877297dbbe4068d708eaa19983f29d6
ehdtjs0612 <inko51366@naver.com>
2024-02-07 20:54:33 +0900

    feat: add login router (test)

--- src/routes/test.routes.ts
+++ src/routes/test.routes.ts
@@ -1,39 +1,36 @@
-import { Router } from 'express';
 import asyncWrap from '../utils/modules/async-wrap.module';
 import { validate } from '../middlewares/validate.middleware';
 import { body, param } from 'express-validator';
 import { ResponseEntity } from '../utils/modules/response-entity.module';
 import { authService } from '../services';
+import { Router } from 'express';
 
 const testRouter = Router();
 
 testRouter.get(
   '/:accountIdx',
-  validate([
-    param('accountIdx')
-      .notEmpty()
-      .withMessage('비어있습니다')
-      .isInt()
-      .withMessage('숫자가 아닙니다')
-      .toInt(),
-    body('name')
-      .notEmpty()
-      .withMessage('비어있습니다')
-      .isLength({ min: 1, max: 6 })
-      .withMessage('1자리부터 6자리까지 가능'),
-    body('email')
-      .notEmpty()
-      .withMessage('비어있습니다')
-      .isEmail()
-      .withMessage('이메일 형식이 아닙니다'),
-  ]),
+  validate([param('accountIdx').notEmpty().isInt().withMessage('정수가 아닙니다').toInt()]),
   asyncWrap(async (req, res, next) => {
     const { accountIdx } = req.params;
-
-    // console.log(typeof userIdx); // number
-    const account = await authService.test(Number(accountIdx));
+    console.log(typeof accountIdx); // number
+    const account = await authService.getUserProfile(Number(accountIdx));
 
     return res.send(ResponseEntity.SUCCESS_WITH(account));
+  }),
+);
+
+testRouter.post(
+  '/login',
+  validate([
+    body('loginId').notEmpty().withMessage('입력되지 않았습니다'),
+    body('password').notEmpty().withMessage('입력되지 않았습니다'),
+  ]),
+  asyncWrap(async (req, res, next) => {
+    const { loginId, password } = req.body;
+
+    const accessToken = await authService.login({ loginId, password });
+
+    return res.send(ResponseEntity.SUCCESS_WITH(accessToken));
   }),
 );
 
