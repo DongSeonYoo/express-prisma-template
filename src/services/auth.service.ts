b51c70a54ed7dfac607f5c9c47095ed6d2a6bb34
ehdtjs0612 <inko51366@naver.com>
2024-02-07 20:55:39 +0900

    feat: add login service (test)

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,13 +1,14 @@
 import { Service } from 'typedi';
 import { Prisma } from '../database/prisma.database';
-import { IAccount } from '../interfaces/IAccount';
+import { IAccount } from '../interfaces/account/IAccount';
+import { ILoginRequest, ILoginResponse } from '../interfaces/account/ILogin';
 import { BadRequestException } from '../utils/modules/custom-error.module';
 
 @Service()
 export class AuthService {
   constructor(private readonly prisma: Prisma) {}
 
-  async test(userIdx: number): Promise<IAccount.IAccountProfile> {
+  async getUserProfile(userIdx: number): Promise<IAccount.IViewProfile.Response> {
     const accountInfo = await this.prisma.accountTb.findUnique({
       where: {
         id: userIdx,
@@ -32,6 +33,31 @@ export class AuthService {
       name: accountInfo.name,
       updatedAt: accountInfo.updatedAt,
       createdAt: accountInfo.createdAt,
+    };
+  }
+
+  async login(loginDto: ILoginRequest): Promise<ILoginResponse> {
+    const findUser = await this.prisma.accountTb.findUnique({
+      where: {
+        loginId: loginDto.loginId,
+      },
+      select: {
+        id: true,
+        password: true,
+      },
+    });
+
+    if (!findUser) {
+      throw new BadRequestException('아이디 또는 비밀번호가 일치하지 않음');
+    }
+
+    // 암호화된 비밀번호 대조..
+    // accessToken 쿵짝쿵짝
+    const accessToken = 'accessToken입미당';
+
+    return {
+      accessToken,
+      userIdx: findUser.id,
     };
   }
 }
