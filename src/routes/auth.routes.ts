e0d36cbb01b8f3077b7c99ab9d1db33f83b54005
ehdtjs0612 <inko51366@naver.com>
2024-02-21 16:29:11 +0900

    feat: add auth routes

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,36 @@
+import asyncWrap from '../utils/modules/async-wrap.module';
+import { validate } from '../middlewares/validate.middleware';
+import { body, param } from 'express-validator';
+import { ResponseEntity } from '../utils/modules/response-entity.module';
+import { authService } from '../services';
+import { Router } from 'express';
+
+const authRouter = Router();
+
+authRouter.get(
+  '/:accountIdx',
+  validate([param('accountIdx').notEmpty().isInt().withMessage('정수가 아닙니다').toInt()]),
+  asyncWrap(async (req, res, next) => {
+    const { accountIdx } = req.params;
+    const account = await authService.getUserProfile(Number(accountIdx));
+
+    return res.send(ResponseEntity.SUCCESS_WITH(account));
+  }),
+);
+
+authRouter.post(
+  '/login',
+  validate([
+    body('loginId').notEmpty().withMessage('입력되지 않았습니다'),
+    body('password').notEmpty().withMessage('입력되지 않았습니다'),
+  ]),
+  asyncWrap(async (req, res, next) => {
+    const { loginId, password } = req.body;
+
+    const accessToken = await authService.login({ loginId, password });
+
+    return res.send(ResponseEntity.SUCCESS_WITH(accessToken));
+  }),
+);
+
+export default authRouter;
