a1209f1a8d2b2c35c986a38d6a5df4a0c67f073a
ehdtjs0612 <inko51366@naver.com>
2024-03-09 21:42:47 +0900

    feat: add check-role function in login-auth middleware

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,11 +2,14 @@ import asyncWrap from '../utils/modules/async-wrap.module';
 import { validate } from '../middlewares/validate.middleware';
 import { param } from 'express-validator';
 import { Router } from 'express';
+import { loginAuthGuard } from '../middlewares/session-guard';
+import { Role } from '../interfaces/IRole';
 
 const authRouter = Router();
 
 authRouter.get(
   '/:accountIdx',
+  loginAuthGuard(),
   validate([param('accountIdx').notEmpty().isInt().withMessage('정수가 아닙니다').toInt()]),
   asyncWrap(async (req, res, next) => {}),
 );
